040f8a7f2ac9cac72fa929a26befa8ef77182dc9
Jack Arnold <jackarnold84@gmail.com>
2023-10-17 21:55:15 -0500

    run updates

--- model/config.py
+++ model/config.py
@@ -18,6 +18,25 @@ leagues = {
                 'score_sd': 24,
                 'team_sd': 9,
             }
+        },
+        'dudes': {
+            'name': 'Dudes',
+            'league_tag': 'dudes',
+            'sport_tag': 'football-2023',
+            'league_id': '447436157',
+            'player_metrics': True,
+            'teams': 10,
+            'playoff_teams': 4,
+            'divisions': False,
+            'weeks_per_playoff_matchup': 2,
+            'total_weeks': 18,
+            'regular_season_weeks': 14,
+            'n_iter': 10000,
+            'model_params': {
+                'score_mean': 122,
+                'score_sd': 24,
+                'team_sd': 9,
+            }
         }
     },
 }
